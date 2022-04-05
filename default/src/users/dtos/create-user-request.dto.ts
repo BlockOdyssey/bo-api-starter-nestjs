a86392fdaa25a974a901535e90ca1297bb6acbd9
blockodyssey-whlee <dokdo2005@blockodyssey.io>
2022-04-05 15:08:53 +0900

    feat: 유저 회원가입 DTO 작성

---
+++ default/src/users/dtos/create-user-request.dto.ts
@@ -0,0 +1,35 @@
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+  Matches,
+} from 'class-validator';
+import { UserRoute } from '../users.constants';
+
+export class CreateUserRequestDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+
+  @Matches(/^01[0179][0-9]{7,8}$/, { message: 'Wrong phone number' })
+  @IsString()
+  @IsNotEmpty()
+  phoneNum: string;
+
+  @IsString()
+  @IsNotEmpty()
+  nickname: string;
+
+  @IsEnum(UserRoute)
+  @IsNotEmpty()
+  userRoute: string;
+}
