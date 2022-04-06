c9457b86f33325d6b11dea7c0d0d3d122c2e6b2c
blockodyssey-whlee <dokdo2005@blockodyssey.io>
2022-04-06 09:52:06 +0900

    feat: JWT 로직 추가

---
+++ default/src/common/jwt/jwt.auth.guard.ts
@@ -0,0 +1,35 @@
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+  ForbiddenException,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtAccessAuthGuard extends AuthGuard('jwt-access') {
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context);
+  }
+
+  handleRequest(err: any, user: any, info: any) {
+    if (err || !user) {
+      throw err || new UnauthorizedException('not valid access token');
+    }
+    return user;
+  }
+}
+
+@Injectable()
+export class JwtRefreshAuthGuard extends AuthGuard('jwt-refresh') {
+  canActivate(context: ExecutionContext) {
+    return super.canActivate(context);
+  }
+
+  handleRequest(err: any, user: any, info: any) {
+    if (err || !user) {
+      throw err || new ForbiddenException('not valid refresh token');
+    }
+    return user;
+  }
+}
---
+++ default/src/common/jwt/jwt.extractor.ts
@@ -0,0 +1,25 @@
+import { Request } from 'express';
+import { JwtFromRequestFunction } from 'passport-jwt';
+import { UnauthorizedException, ForbiddenException } from '@nestjs/common';
+
+export const jwtAccessExtractor: JwtFromRequestFunction = (
+  request: Request,
+): string | null => {
+  try {
+    const accessToken = request.header('Authorization').split(' ')[1];
+    return accessToken;
+  } catch (error) {
+    throw new UnauthorizedException('not authenticated');
+  }
+};
+
+export const jwtRefreshExtractor: JwtFromRequestFunction = (
+  request: Request,
+): string | null => {
+  try {
+    const refreshToken = request.cookies.refreshToken;
+    return refreshToken;
+  } catch (error) {
+    throw new ForbiddenException('not authenticated');
+  }
+};
