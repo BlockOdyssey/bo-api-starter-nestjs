224c9b63ac42166deedcded1467b8e2717ebedc2
blockodyssey-whlee <dokdo2005@blockodyssey.io>
2022-04-06 13:41:42 +0900

    mod: try-catch 추가

--- default/src/users/users.service.ts
+++ default/src/users/users.service.ts
@@ -1,10 +1,15 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthService } from 'src/auth/auth.service';
 import { CreateUserRequestDto } from './dtos/create-user-request.dto';
 import { CreateUserResponseDto } from './dtos/create-user-response.dto';
 import { LoginRequestDto } from './dtos/login-request.dto';
 import { LoginResponseDto } from './dtos/login-response.dto';
+import { Users } from './entities/users.entity';
 import { UsersRepository } from './repositories/users.repository';
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
@@ -18,15 +23,58 @@ export class UsersService {
   ) {}
 
   async userLogin(loginInfo: LoginRequestDto): Promise<LoginResponseDto> {
-    const loginResult = await this.usersRepository.userLogin(loginInfo.email);
-    if (!loginResult) throw new ConflictException('wrong email address');
+    try {
+      const loginResult = await this.usersRepository.userLogin(loginInfo.email);
+      if (!loginResult) throw new ConflictException('wrong email address');
 
-    const checkPassword = await bcrypt.compare(
-      loginInfo.password,
-      loginResult.password,
-    );
-    if (checkPassword) {
-      const tokenPayload = { sub: loginResult.id };
+      const checkPassword = await bcrypt.compare(
+        loginInfo.password,
+        loginResult.password,
+      );
+      if (checkPassword) {
+        const tokenPayload = { sub: loginResult.id };
+        const accessToken = await this.authService.createAccessToken(
+          tokenPayload,
+        );
+        const refreshToken = await this.authService.createRefreshToken(
+          tokenPayload,
+        );
+
+        await this.usersRepository.update(
+          { id: loginResult.id },
+          { accessToken, refreshToken },
+        );
+
+        return await this.usersRepository.getUserResult(loginResult.id);
+      } else {
+        throw new ConflictException('wrong password');
+      }
+    } catch (error) {
+      throw new InternalServerErrorException('server error');
+    }
+  }
+
+  async joinUser(
+    userInfo: CreateUserRequestDto,
+  ): Promise<CreateUserResponseDto> {
+    try {
+      const checkEmail = await this.usersRepository.checkEmailDuplicates(
+        userInfo.email,
+      );
+      if (checkEmail) throw new ConflictException('email address duplicates');
+
+      const checkPhoneNum = await this.usersRepository.checkPhoneNumDuplicates(
+        userInfo.phoneNum,
+      );
+      if (checkPhoneNum) throw new ConflictException('phone number duplicates');
+
+      const newPassword = await bcrypt.hashSync(userInfo.password, saltRounds);
+      const createResult = await this.usersRepository.save({
+        ...userInfo,
+        password: newPassword,
+      });
+
+      const tokenPayload = { sub: createResult.id };
       const accessToken = await this.authService.createAccessToken(
         tokenPayload,
       );
@@ -35,47 +83,14 @@ export class UsersService {
       );
 
       await this.usersRepository.update(
-        { id: loginResult.id },
+        { id: createResult.id },
         { accessToken, refreshToken },
       );
 
-      return await this.usersRepository.getUserResult(loginResult.id);
-    } else {
-      throw new ConflictException('wrong password');
+      return await this.usersRepository.getUserResult(createResult.id);
+    } catch (error) {
+      throw new InternalServerErrorException('server error');
     }
-  }
-
-  async joinUser(
-    userInfo: CreateUserRequestDto,
-  ): Promise<CreateUserResponseDto> {
-    const checkEmail = await this.usersRepository.checkEmailDuplicates(
-      userInfo.email,
-    );
-    if (checkEmail) throw new ConflictException('email address duplicates');
-
-    const checkPhoneNum = await this.usersRepository.checkPhoneNumDuplicates(
-      userInfo.phoneNum,
-    );
-    if (checkPhoneNum) throw new ConflictException('phone number duplicates');
-
-    const newPassword = await bcrypt.hashSync(userInfo.password, saltRounds);
-    const createResult = await this.usersRepository.save({
-      ...userInfo,
-      password: newPassword,
-    });
-
-    const tokenPayload = { sub: createResult.id };
-    const accessToken = await this.authService.createAccessToken(tokenPayload);
-    const refreshToken = await this.authService.createRefreshToken(
-      tokenPayload,
-    );
-
-    await this.usersRepository.update(
-      { id: createResult.id },
-      { accessToken, refreshToken },
-    );
-
-    return await this.usersRepository.getUserResult(createResult.id);
   }
 
   async userLogout() {}
