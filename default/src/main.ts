73acb5c6cfade854bcbe76539ee179ba1a264a47
blockodyssey-whlee <dokdo2005@blockodyssey.io>
2022-06-08 13:42:00 +0900

    chore: cookie-parser 삭제

--- default/src/main.ts
+++ default/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as config from 'config';
-import * as cookieParser from 'cookie-parser';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { ValidationPipe } from '@nestjs/common';
 
@@ -10,7 +9,6 @@ async function bootstrap() {
   const { port } = serverConfig;
 
   const app = await NestFactory.create(AppModule);
-  app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(port);
