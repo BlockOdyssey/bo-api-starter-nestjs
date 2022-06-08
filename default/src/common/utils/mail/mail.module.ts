3b0f687518b3718ed549852273218237bad565a6
blockodyssey-whlee <dokdo2005@blockodyssey.io>
2022-06-08 13:38:58 +0900

    feat: mailer 추가

---
+++ default/src/common/utils/mail/mail.module.ts
@@ -0,0 +1,41 @@
+import { Module } from '@nestjs/common';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { EjsAdapter } from '@nestjs-modules/mailer/dist/adapters/ejs.adapter';
+import * as config from 'config';
+import { MailService } from './mail.service';
+
+const mailConfig = config.get('mail');
+const { mailUser, mailPassword } = mailConfig;
+
+@Module({
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: () => {
+        return {
+          transport: {
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            auth: {
+              user: mailUser,
+              pass: mailPassword,
+            },
+          },
+          defaults: {
+            from: '"M Station" <noreply@blockodyssey.io>',
+          },
+          template: {
+            dir: __dirname + '/templates',
+            adapter: new EjsAdapter(),
+            options: {
+              strict: true,
+            },
+          },
+        };
+      },
+    }),
+  ],
+  providers: [MailService],
+  exports: [MailService],
+})
+export class MailModule {}
